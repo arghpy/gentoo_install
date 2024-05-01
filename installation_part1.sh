06fed05e943fba9df272c9df0e318a225f47e866
arghpy <arghpy@i3>
2024-05-01 10:45:31 +0300

    update repository

---
+++ installation_part1.sh
@@ -0,0 +1,248 @@
+#!/usr/bin/env bash
+
+LOG_FUNC="https://raw.githubusercontent.com/arghpy/functions/main/log_functions.sh"
+SECOND_SCRIPT="https://raw.githubusercontent.com/arghpy/gentoo_install/main/installation_part2.sh"
+
+# Sourcing log functions
+wget "${LOG_FUNC}"
+if source log_functions.sh; then
+    log_info "sourced log_functions.sh"
+else
+    echo "Error! Could not source log_functions.sh"
+    exit -1
+fi
+
+# Downloading the second part of installation
+if wget "${SECOND_SCRIPT}"; then
+    log_info "Downloaded the second part of the installation"
+else
+    log_error "Couldn't download the second part of the installation. Aborting..."
+    exit -1
+fi
+
+# Check for internet
+check_internet() {
+    log_info "Check Internet"
+	if ! ping -c1 -w1 8.8.8.8 > /dev/null 2>&1; then
+        log_error "No Internet Connection"
+        log_info "Visit https://wiki.gentoo.org/wiki/Handbook:AMD64/Installation/Networking"
+        log_info "Optionally use 'links https://wiki.gentoo.org/wiki/Handbook:AMD64/Installation/Networking'"
+        exit -1
+    else
+        log_ok "Connected to internet"
+	fi
+}
+
+# Selecting the disk to install on
+disks() {
+    log_info "Select installation disk"
+    LIST="$(lsblk -d -n | grep -v "loop" | awk '{print $1, $4}' | nl -s") ")"
+    echo "${LIST}"
+    OPTION=""
+
+    while [[ -z "$(echo "${LIST}" | grep "  ${OPTION})")" ]]; do
+        printf "Choose a disk (e.g.: 1): "
+        read OPTION
+    done
+
+    DISK="$(echo "${LIST}" | grep "  ${OPTION})" | awk '{print $2}')"
+
+    log_ok "DONE"
+}
+
+# Creating partitions
+partitioning() {
+    log_info "Partitioning disk"
+    if [[ -n $(ls /sys/firmware/efi/efivars 2>/dev/null) ]];then
+
+        MODE="UEFI"
+
+        parted --script /dev/"${DISK}" mklabel gpt
+
+        parted --script /dev/"${DISK}" mkpart fat32 2048s 1GiB
+        parted --script /dev/"${DISK}" set 1 esp on
+
+        parted --script /dev/"${DISK}" mkpart linux-swap 1GiB 5GiB
+        parted --script /dev/"${DISK}" mkpart ext4 5GiB 35GiB
+        parted --script /dev/"${DISK}" mkpart ext4 35GiB 100%
+        parted --script /dev/"${DISK}" align-check optimal 1 
+    else
+
+        MODE="BIOS"
+
+        parted --script /dev/"${DISK}" mklabel msdos
+        parted --script /dev/"${DISK}" mkpart primary ext4 2048s 35GiB
+        parted --script /dev/"${DISK}" mkpart primary linux-swap 35GiB 39GiB
+        parted --script /dev/"${DISK}" mkpart primary ext4 39GiB 100%
+        parted --script /dev/"${DISK}" align-check optimal 1 
+    fi
+
+    log_ok "DONE"
+
+}
+
+
+# Formatting partitions
+formatting() {
+
+    log_info "Formatting partitions"
+    PARTITIONS=$(lsblk --list --noheadings /dev/"${DISK}" | tail -n +2 | awk '{print $1}')
+
+    if [[ "${MODE}" == "UEFI" ]]; then
+
+        BOOT_P=$(echo "$PARTITIONS" | sed -n '1p')
+        mkfs.vfat -F32 /dev/"${BOOT_P}"
+
+        SWAP_P=$(echo "$PARTITIONS" | sed -n '2p')
+        ROOT_P=$(echo "$PARTITIONS" | sed -n '3p')
+        HOME_P=$(echo "$PARTITIONS" | sed -n '4p')
+    elif [[ "${MODE}" == "BIOS" ]]; then 
+        ROOT_P=$(echo "$PARTITIONS" | sed -n '1p')
+        SWAP_P=$(echo "$PARTITIONS" | sed -n '2p')
+        HOME_P=$(echo "$PARTITIONS" | sed -n '3p')
+
+    fi
+
+    mkswap /dev/"${SWAP_P}"
+    swapon /dev/"${SWAP_P}"
+    mkfs.ext4 -F /dev/"${HOME_P}"
+    mkfs.ext4 -F /dev/"${ROOT_P}"
+
+    log_ok "DONE"
+}
+
+
+# Mounting partitons
+mounting() {
+    log_info "Mounting partitions"
+
+    mkdir --parents /mnt/gentoo
+    mount /dev/"${ROOT_P}" /mnt/gentoo
+
+    mkdir --parents /mnt/gentoo/home
+    mount /dev/"${HOME_P}" /mnt/gentoo/home
+
+    log_ok "DONE"
+}
+
+# Configuring date
+date_config() {
+    log_info "Configuring time with chrony"
+    chronyd -q
+    log_ok "DONE"
+}
+
+# Downloading and unarchiving stage3 tarball
+download_and_configure_stage3() {
+    log_info "Downloading and setting stage3 tarball"
+    STAGE="https://distfiles.gentoo.org/releases/amd64/autobuilds/20230827T170145Z/stage3-amd64-openrc-20230827T170145Z.tar.xz"
+
+    pushd /mnt/gentoo
+
+    wget "${STAGE}"
+    tar xpf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
+    rm stage3-*.tar.xz
+
+    popd
+
+    log_ok "DONE"
+}
+
+# Configuring /etc/portage/make.conf
+make_conf_portage() {
+    log_info "Configuring /etc/portage/make.conf"
+
+    MAKE_CONF="/mnt/gentoo/etc/portage/make.conf"
+
+    COMMON_FLAGS_OLD="$(grep "^COMMON_FLAGS=" "${MAKE_CONF}")"
+    COMMON_FLAGS_NEW='COMMON_FLAGS="-march=native -O2 -pipe"'
+    CORES="$(nproc)"
+    MAKEOPTS="MAKEOPTS=\"-j${CORES}\""
+    USE='USE="X matroska blueray archive fontconfig truetype xml x264 x265 minimal postproc dbus acl alsa grub pulseaudio networkmanager -gnome -kde"'
+
+    # Changing COMMON_FLAGS
+    sed -i "s|${COMMON_FLAGS_OLD}|${COMMON_FLAGS_NEW}|g" "${MAKE_CONF}"
+
+    # Appending MAKEOPTS
+    echo "${MAKEOPTS}" >> "${MAKE_CONF}"
+    echo "${USE}" >> "${MAKE_CONF}"
+
+    log_ok "DONE"
+}
+
+# Selecting mirrors
+select_mirrors() {
+    log_info "Selecting mirrors"
+
+    mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
+
+    log_ok "DONE"
+}
+
+# Configure Gentoo ebuild repository
+config_ebuild_repo() {
+    log_info "Configure Gentoo ebuild repository"
+
+    mkdir --parents /mnt/gentoo/etc/portage/repos.conf
+    cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+
+    log_ok "DONE"
+}
+
+# Copying DNS info
+dns_copy() {
+    log_info "Copying DNS info"
+
+    cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+
+    log_ok "DONE"
+}
+
+# Mounting the necessary filesystems
+mounting_filesystems() {
+    log_info "Mounting the necessary filesystems"
+
+    mount --types proc /proc /mnt/gentoo/proc
+    mount --rbind /sys /mnt/gentoo/sys
+    mount --make-rslave /mnt/gentoo/sys
+    mount --rbind /dev /mnt/gentoo/dev
+    mount --make-rslave /mnt/gentoo/dev
+    mount --bind /run /mnt/gentoo/run
+    mount --make-slave /mnt/gentoo/run
+
+    log_ok "DONE"
+}
+
+# Enter the new environment
+enter_environment() {
+    log_info "Copying the second installation part to the new environment"
+    chmod +x installation_part2.sh
+    cp installation_part2.sh /mnt/gentoo/
+    log_ok "DONE"
+
+    log_info "Removing installation_part1.sh and log_functions.sh"
+    log_info "Entering the new environment"
+    log_info "Run the second part of the script: './installation_part2.sh ${MODE} ${DISK}'"
+    rm -f installation_part1.sh log_functions.sh
+    chroot /mnt/gentoo /bin/bash
+}
+
+
+# Main function to run all program
+main() {
+    check_internet
+    disks
+    partitioning
+    formatting
+    mounting
+    date_config
+    download_and_configure_stage3
+    make_conf_portage
+    select_mirrors
+    config_ebuild_repo
+    dns_copy
+    mounting_filesystems
+    enter_environment
+}
+
+main
---
+++ installation_part2.sh
@@ -0,0 +1,385 @@
+#!/usr/bin/env bash
+
+MODE="${1}"
+DISK="${2}"
+
+LOG_FUNC="https://raw.githubusercontent.com/arghpy/functions/main/log_functions.sh"
+DEP_FILE="https://raw.githubusercontent.com/arghpy/gentoo_install/main/dependencies.txt"
+
+# Sourcing log functions
+wget "${LOG_FUNC}"
+if source log_functions.sh; then
+    log_info "sourced log_functions.sh"
+else
+    echo "Error! Could not source log_functions.sh"
+    exit -1
+fi
+
+# Preparing environment
+prep_env() {
+    log_info "Preparing environment"
+    source /etc/profile
+    export PS1="(chroot) ${PS1}"
+    log_ok "DONE"
+}
+
+# Mounting boot partition
+mount_boot() {
+    log_info "Mounting boot partition"
+
+    PARTITIONS=$(lsblk --list --noheadings /dev/"${DISK}" | tail -n +2 | awk '{print $1}')
+    BOOT_P=$(echo "$PARTITIONS" | sed -n '1p')
+
+    [[ "${MODE}" == "UEFI" ]] && mount /dev/"${BOOT_P}" /boot
+
+    log_ok "DONE"
+}
+
+# Configure portage
+configure_portage() {
+    log_info "Configure portage"
+
+    log_info "Installing a Gentoo ebuild repository snapshot from the web"
+    emerge-webrsync
+    log_ok "DONE"
+
+    log_info "Updating the @world set"
+    emerge --quiet --update --deep --newuse @world
+    log_ok "DONE"
+
+    log_info "Configuring CPU_FLAGS"
+    emerge --quiet app-portage/cpuid2cpuflags
+    echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
+    log_ok "DONE"
+
+    log_info "Configuring VIDEO_CARDS"
+
+    GPU="$(lspci | grep VGA)"
+
+    if [[ $(echo "${GPU}" | grep -q -i intel; echo $?) == 0 ]]; then
+        echo 'VIDEO_CARDS="intel"' >> /etc/portage/make.conf
+    elif [[ "$(echo "${GPU}" | grep -q -i nvidia; echo $?)" == 0 ]]; then 
+        echo 'VIDEO_CARDS="nouveau"' >> /etc/portage/make.conf
+        emerge --quiet x11-drivers/nvidia-drivers
+    elif [[ "$(echo "${GPU}" | grep -q -i amd; echo $?)" == 0 ]]; then 
+        echo 'VIDEO_CARDS="radeon"' >> /etc/portage/make.conf
+    fi
+
+
+    log_ok "DONE"
+    log_ok "DONE"
+}
+
+# Setting the timezone
+setting_timezone() {
+    log_info "Setting the timezone"
+    
+    echo "Europe/Bucharest" > /etc/timezone
+    emerge --config sys-libs/timezone-data
+
+    log_ok "DONE"
+}
+
+# Configuring locales
+configure_locales() {
+    log_info "Configuring locales"
+
+    echo "en_US ISO-8859-1" >> /etc/locale.gen
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+    
+    # create file
+    echo 'LANG="en_US.UTF-8"' > /etc/env.d/02locale
+    
+    # append to file
+    echo 'LC_COLLATE="en_US.UTF-8"' > /etc/env.d/02locale
+
+    env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
+
+    log_ok "DONE"
+}
+
+# Configure and install kernel
+configure_and_install_kernel() {
+    log_info "Configure and install kernel"
+    # create file
+    echo "# Accepting the license for linux-firmware" > /etc/portage/package.license
+
+    # append to file
+    echo "sys-kernel/linux-firmware linux-fw-redistributable" >> /etc/portage/package.license
+    echo "" >> /etc/portage/package.license
+    echo "# Accepting any license that permits redistribution" >> /etc/portage/package.license
+    echo "sys-kernel/linux-firmware @BINARY-REDISTRIBUTABLE" >> /etc/portage/package.license
+
+
+    log_info "Installing linux-firmware"
+    emerge --quiet sys-kernel/linux-firmware
+    if [[ $(lscpu | grep "^Model name" | grep -q -i intel; echo $?) == 0 ]]; then
+        emerge --quiet sys-firmware/intel-microcode
+        emerge --quiet x11-drivers/xf86-video-intel
+    fi
+    log_ok "DONE"
+
+    log_info "Installing the kernel"
+    emerge --quiet sys-kernel/installkernel-gentoo
+    emerge --quiet sys-kernel/gentoo-kernel
+    log_ok "DONE"
+
+    log_info "Cleaning up"
+    emerge --depclean
+    log_ok "DONE"
+
+    log_ok "DONE"
+}
+
+# Generating the fstab
+generate_fstab() {
+    log_info "Generating the fstab"
+    emerge --quiet sys-fs/genfstab
+    genfstab -U / >> /etc/fstab
+    log_ok "DONE"
+}
+
+# Generate hostname
+generate_hostname() {
+    log_info "Generate hostname"
+    echo "gentoo" > /etc/hostname
+    log_ok "DONE"
+}
+
+# Enable networking
+enable_network() {
+    log_info "Enable networking"
+    emerge --quiet net-misc/networkmanager
+    for x in /etc/runlevels/default/net.* ; do rc-update del $(basename $x) default ; rc-service --ifstarted $(basename $x) stop; done
+    rc-update del dhcpcd default
+    rc-service NetworkManager start
+    rc-update add NetworkManager default
+    log_ok "DONE"
+}
+
+# Change root password
+change_root_password() {
+    log_info "Change root password"
+    while ! passwd ; do
+        sleep 1
+    done
+    log_ok "DONE"
+}
+
+# Installing tools
+install_tools() {
+    log_info "Installing tools"
+    log_info "system logger"
+    emerge --quiet app-admin/sysklogd
+    rc-update add sysklogd default
+    log_ok "DONE"
+
+    log_info "Cron daemon"
+    emerge --quiet sys-process/cronie
+    rc-update add cronie default
+    log_ok "DONE"
+
+    log_info "File indexing"
+    emerge --quiet sys-apps/mlocate
+    log_ok "DONE"
+
+    log_info "enabling sshd"
+    rc-update add sshd default
+    log_ok "DONE"
+
+    log_info "bash completion"
+    emerge --quiet app-shells/bash-completion
+    log_ok "DONE"
+
+    log_info "Time Synchronization"
+    emerge --quiet net-misc/chrony
+    rc-update add chronyd default
+    log_ok "DONE"
+
+    log_info "udev scheduler"
+    emerge --quiet sys-block/io-scheduler-udev-rules
+    log_ok "DONE"
+
+    log_info "Wireless tools"
+    emerge --quiet net-wireless/iw net-wireless/wpa_supplicant
+    log_ok "DONE"
+    log_ok "DONE"
+}
+
+# Install packages
+install_packages() {
+    log_info "Install packages"
+    log_info "Installing rust-bin"
+    emerge --quiet rust-bin
+    log_ok "DONE"
+    wget "${DEP_FILE}"
+    DEPLIST="$(cat dependencies.txt | grep -v "#" | paste -sd" ")"
+    emerge --autounmask-continue --quiet ${DEPLIST}
+    log_ok "DONE"
+}
+
+# Install pamixer
+install_pamixer() {
+    log_info "Installing pamixer"
+    git clone https://github.com/cdemoulins/pamixer
+    pushd pamixer
+    meson setup build
+    meson compile -C build
+    meson install -C build
+    log_ok "DONE"
+}
+
+# Installing grub and creating configuration
+grub_configuration() {
+    log_info "Installing and configuring grub"
+	if [[ "${MODE}" == "UEFI" ]]; then
+        echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
+        emerge --quiet sys-boot/grub
+        grub-install --target=x86_64-efi --efi-directory=/boot
+		grub-mkconfig -o /boot/grub/grub.cfg
+	elif [[ "${MODE}" == "BIOS" ]]; then
+        emerge --quiet sys-boot/grub
+		grub-install /dev/"${DISK}"
+		grub-mkconfig -o /boot/grub/grub.cfg
+	else
+		log_error "An error occured at grub step. Exiting..."
+		exit -1
+	fi
+    log_ok "DONE"
+}
+
+# Set user and password
+set_user() {
+
+    log_info "Setting user account"
+
+    NAME=""
+
+    while [ -z "${NAME}" ]; do
+        printf "Enter name for the local user: "
+        read NAME
+    done
+
+    log_info "Adding user to users, audio, video and wheel group"
+	useradd -m -G wheel,users,audio,video -s /bin/bash "${NAME}"
+
+    log_info "Adding wheel to sudoers"
+    echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+    log_info "Setting up user password"
+    while ! passwd "${NAME}"; do
+        sleep 1
+    done
+
+    log_ok "DONE"
+}
+
+# Move configuration files in user home
+my_configuration() {
+    log_info "Configuring the user's home directory"
+    CONFIG_GIT="https://github.com/arghpy/dotfiles"
+    rm -rf /home/"${NAME}"/* 
+    rm -rf /home/"${NAME}"/.* 
+    git -C /home/"${NAME}"/ clone "${CONFIG_GIT}"
+    mv /home/"${NAME}"/dotfiles/* /home/"${NAME}"/.
+    mv /home/"${NAME}"/dotfiles/.* /home/"${NAME}"/.
+    rm -rf /home/"${NAME}"/dotfiles
+    rm -rf /home/"${NAME}"/.git
+    log_ok "DONE"
+}
+
+# Copy custom dwm suite and neovim config
+my_custom_progs() {
+    mkdir --parents /home/"${NAME}"/.local/src/
+    mkdir --parents /home/"${NAME}"/.config
+    log_info "Cloning dwm in .local/src"
+    git -C /home/"${NAME}"/.local/src/ clone "https://github.com/arghpy/dwm"
+    rm -rf /home/"${NAME}"/.local/src/dwm/.git
+    log_ok "DONE"
+
+    log_info "Cloning dwmblocks in .local/src"
+    git -C /home/"${NAME}"/.local/src/ clone "https://github.com/arghpy/dwmblocks"
+    rm -rf /home/"${NAME}"/.local/src/dwmblocks/.git
+    log_ok "DONE"
+
+    log_info "Cloning st in .local/src"
+    git -C /home/"${NAME}"/.local/src/ clone "https://github.com/arghpy/st"
+    rm -rf /home/"${NAME}"/.local/src/st/.git
+    log_ok "DONE"
+
+    log_info "Cloning nvim in .config"
+    git -C /home/"${NAME}"/.config/ clone "https://github.com/arghpy/nvim_config"
+    mv /home/"${NAME}"/.config/nvim_config/* /home/"${NAME}"/.config/
+    rm -rf /home/"${NAME}"/.config/nvim_config
+    rm -rf /home/"${NAME}"/.config/.git
+    log_ok "DONE"
+
+    log_info "Modifying config settings for the local user"
+    for i in $(grep -rl "arghpy" /home/"${NAME}"/.* /home/"${NAME}"/* 2>/dev/null); do
+        sed -i "s|arghpy|${NAME}|g" "${i}"
+    done
+    log_ok "DONE"
+
+    log_info "Compiling sources in .local/src/"
+    for i in $(ls -d /home/"${NAME}"/.local/src/*);do
+        pushd "${i}"
+        make clean install
+        popd
+    done
+
+    chown -R  "${NAME}":wheel /home/"${NAME}"/* /home/"${NAME}"/.*
+
+    rc-update add elogind boot
+    usermod -s /bin/zsh -aG plugdev "${NAME}"
+    CORES="$(nproc)"
+    NEW_CORES="$((CORES / 2))"
+    sed -E -i "s|MAKEOPTS=.*|MAKEOPTS=\"-j${NEW_CORES}\"|g" /etc/portage/make.conf
+    log_ok "DONE"
+}
+
+set_fonts() {
+    log_info "Setting JetBrains Mono Nerd Font"
+    pushd /usr/share/fonts
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+    unzip JetBrainsMono.zip
+    rm JetBrainsMono.zip
+    popd
+    log_ok "DONE"
+}
+
+# Clean up
+clean_up() {
+    log_info "Clean up"
+    rm -rf /dependencies.txt /log_functions.sh /pamixer
+    log_ok "DONE"
+}
+
+# Main function to run all program
+main() {
+    prep_env
+    mount_boot
+    change_root_password
+    set_user
+    configure_portage
+    setting_timezone
+    configure_locales
+    configure_and_install_kernel
+    generate_fstab
+    generate_hostname
+    enable_network
+    install_tools
+    install_packages
+    install_pamixer
+    grub_configuration
+    my_configuration
+    my_custom_progs
+    set_fonts
+    clean_up
+
+    log_ok "DONE"
+    log_info "Exit the chroot now 'exit' and reboot"
+    log_warning "Don't forget to take out the installation media"
+}
+
+main
